43fd681eb263d8c697b59b6f5d94da4a94183d4d
= <=>
2019-04-26 10:15:56 +0200

    get method completed and test.js file for better testing

--- test/test.js
+++ test/test.js
@@ -13,28 +13,37 @@ http.createServer((request, response) => {
     response.on('error', (err) => {
       console.error(err);
     });
-
+    const fileType = require('file-type')
+    var fs = require('fs')
+    
     if(method === 'GET' && url.indexOf('/get/') !== -1){
         const hash = url.replace('/get/','',url)
         if(hash.length === 46){
             const IPFS = require('ipfs')
             const node = new IPFS({ repo: '~/.ipfs' })
-            node.on('ready', () => {
-              if(hash){ 
-                node.cat(hash, function (err, file) {
-                    if (err) {
-                        throw err
+            // UPLOAD A FILE FIRST
+              fs.readFile('document.pdf', function(error, content){
+                node.on('ready', () => {
+                  node.add(content).then(results => {
+                    const hash = results[0].hash
+                    console.log(hash)
+                    //THEN READ HASH AND SERVE FILE FROM NODEJS
+                    if(hash){ 
+                      node.cat(hash, function (err, file) {
+                          if (err) {
+                              throw err
+                          }
+                          //READ CORRECT MIMETYPE AND SET TO HEADER RESPONSE
+                          var mimetype = fileType(file)
+                          response.setHeader('Content-Type', mimetype.mime);
+                          response.end(file)
+                          node.stop()
+                      })
                     }
-                    const fileType = require('file-type')
-                    var mimetype = fileType(file)
-                    if(mimetype){
-                      response.setHeader('Content-Type', mimetype.mime);
-                    }
-                    response.end(file)
-                    node.stop()
+                  })
                 })
-              }
-            })
+
+              })
 
               /*
 
