12715202da4b2cb1705bab6b6adc1b2bd0eb1621
= <=>
2019-04-26 12:09:37 +0200

    min fixes

--- ipfs.js
+++ ipfs.js
@@ -5,72 +5,72 @@ const fileType = require('file-type')
 
 var app = express()
 
-app.get('/', function (req, res){
-  res.send('IPFS ONLINE');
-});
-
 app.post('/add', function (req, res){
-  var form = new formidable.IncomingForm();
-
-  form.parse(req);
-
-  form.on('file', function (name, file){
-      console.log('UPLOADING FROM ' + file.path + ' TO IPFS');
-      fs.readFile(file.path, function(error, content){
-        const IPFS = require('ipfs')
-        const node = new IPFS({ repo: '~/.ipfs' })
-        node.on('ready', () => {
-          node.add(content).then(results => {
-            const hash = results[0].hash
-            res.send({
-              data: {
-                hash: hash,
-                filename: file.name,
-                type: file.type
-              },
-              status: 200
+  setTimeout(function () {
+    var form = new formidable.IncomingForm();
+    form.parse(req);
+    form.on('file', function (name, file){
+        console.log('UPLOADING FROM ' + file.path + ' TO IPFS');
+        fs.readFile(file.path, function(error, content){
+          const IPFS = require('ipfs')
+          const node = new IPFS({ repo: '~/.ipfs' })
+          node.on('ready', () => {
+            node.add(content).then(results => {
+              const hash = results[0].hash
+              res.send({
+                data: {
+                  hash: hash,
+                  filename: file.name,
+                  type: file.type
+                },
+                status: 200
+              })
+              node.stop()
             })
-            node.stop()
           })
         })
-      })
-  });
+    });
+  }, 10 * Math.floor((Math.random() * 10) + 1))
 });
 
 app.get('/get/:hash', function (req, res){
-  const IPFS = require('ipfs')
-  const node = new IPFS({ repo: '~/.ipfs' })
-  const hash = req.params.hash
+  setTimeout(function () {
+    const IPFS = require('ipfs')
+    const node = new IPFS({ repo: '~/.ipfs' })
+    const hash = req.params.hash
 
-  node.on('ready', () => {
-    node.cat(hash, function (err, file) {
-      if (err) {
-          throw err
-      }
-      var mimetype = fileType(file)
-      res.setHeader('Content-Type', mimetype.mime);
-      res.end(file)
-      node.stop()
+    node.on('ready', () => {
+      node.cat(hash, function (err, file) {
+        if (err) {
+            throw err
+        }
+        var mimetype = fileType(file)
+        res.setHeader('Content-Type', mimetype.mime);
+        res.end(file)
+        node.stop()
+      })
     })
-  })
+  }, 10 * Math.floor((Math.random() * 10) + 1))
 });
 
 app.get('/readpin', function (req, res){
-  const IPFS = require('ipfs')
-  const node = new IPFS({ repo: '~/.ipfs' })
+  setTimeout(function(){
+    const IPFS = require('ipfs')
+    const node = new IPFS({ repo: '~/.ipfs' })
 
-  node.on('ready', () => {
-    node.pin.ls({ type: 'recursive' }, function (err, pinset) {
-      if (err) {
-        throw err
-      }
-      res.send({
-        data: pinset,
-        status: 200
+    node.on('ready', () => {
+      node.pin.ls({ type: 'recursive' }, function (err, pinset) {
+        if (err) {
+          throw err
+        }
+        res.send({
+          data: pinset,
+          status: 200
+        })
+        node.stop()
       })
-      node.stop()
     })
-  })
+  }, 10 * Math.floor((Math.random() * 10) + 1))
 });
 
 app.listen(3000);
