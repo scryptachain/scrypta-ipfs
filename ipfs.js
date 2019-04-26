06d5eb27c76b78a5a029923546174a7070e6fc8d
= <=>
2019-04-26 14:34:06 +0200

    verify endpoint

--- ipfs.js
+++ ipfs.js
@@ -30,6 +30,31 @@ app.post('/add', function (req, res){
   }, 10 * Math.floor((Math.random() * 10) + 1))
 });
 
+app.post('/verify/:hash', function (req, res){
+  setTimeout(function () {
+    var hash = req.params.hash
+    var form = new formidable.IncomingForm();
+    form.parse(req)
+    form.on('file', function (name, file){
+        fs.readFile(file.path, {onlyHash: true}, function(error, content){
+          const IPFS = require('ipfs')
+          const node = new IPFS({ repo: '~/.ipfs' })
+          node.on('ready', () => {
+            node.add(content).then(results => {
+              var calculated = results[0].hash
+              if(calculated !== hash){
+                res.send(false)
+              }else{
+                res.send(true)
+              }
+              node.stop()
+            })
+          })
+        })
+    });
+  }, 10 * Math.floor((Math.random() * 10) + 1))
+});
+
 app.get('/get/:hash', function (req, res){
   setTimeout(function () {
     const IPFS = require('ipfs')
